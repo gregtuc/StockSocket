2950a334765973c9302fa071e6ff7ab70aad9f64
Greg <60011793+gregtuc@users.noreply.github.com>
2021-06-10 11:06:35 -0400

    refactoring protobuf decoding (#29)
    * Removing finstreamer and adding in smaller implementation
    
    * Updating Tests
    
    * updating dependency
    
    * updating version number
    
    * Refactoring

--- src/StockSocket.js
+++ src/StockSocket.js
@@ -1,5 +1,7 @@
-"use strict";
-const protobuf = require("./__finStreamer-proto");
+const protobuf = require("protobufjs");
+const root = protobuf.loadSync(__dirname + "/PricingData.proto");
+const Ticker = root.lookupType("ticker");
+
 const WebSocket = require("isomorphic-ws");
 var ws = new WebSocket("wss://streamer.finance.yahoo.com");
 var tickersArray = [];
@@ -9,7 +11,7 @@ var tickersArray = [];
  * @param {String[]} tickers An array of strings
  * @param {*} callback
  */
-async function addTickers(tickers, callback) {
+function addTickers(tickers, callback) {
   if (!Array.isArray(tickers)) {
     throw "You must add multiple tickers with the addTickers method.";
   }
@@ -25,7 +27,7 @@ async function addTickers(tickers, callback) {
  * @param {String} ticker A string containing a ticker symbol
  * @param {Function} callback A callback method
  */
-async function addTicker(ticker, callback) {
+function addTicker(ticker, callback) {
   if (Array.isArray(ticker)) {
     throw "You can only add one ticker with the addTickers method.";
   }
@@ -38,7 +40,7 @@ async function addTicker(ticker, callback) {
  * Method that removes a specific ticker from the watchlist
  * @param {string} ticker A string containing a ticker symbol
  */
-async function removeTicker(ticker) {
+function removeTicker(ticker) {
   for (var i = 0; i < tickersArray.length; i++) {
     if (tickersArray[i] == ticker) {
       tickersArray.splice(i, 1);
@@ -50,7 +52,7 @@ async function removeTicker(ticker) {
  * Method that removes an array of tickers from the watchlist
  * @param {String[]} tickers An array of strings
  */
-async function removeTickers(tickers) {
+function removeTickers(tickers) {
   for (var i = 0; i < tickers.length; i++) {
     for (var j = 0; j < tickersArray.length; j++) {
       if (tickersArray[j] == tickers[i]) {
@@ -63,7 +65,7 @@ async function removeTickers(tickers) {
 /**
  * Method that removes every element from the tickers watchlist
  */
-async function removeAllTickers() {
+function removeAllTickers() {
   for (var i = 0; i < tickersArray.length; i++) {
     tickersArray.splice(i, 1);
   }
@@ -74,33 +76,24 @@ async function removeAllTickers() {
  * @param {Object} input A string or array of strings
  * @param {Function} callback A callback method
  */
-async function startDataFeed(callback) {
+function startDataFeed(callback) {
   //Close existing connections and re-open with all tickers.
   if (ws.readyState == 1) {
     ws.send("close");
     ws = new WebSocket("wss://streamer.finance.yahoo.com");
   }
 
-  //Format opening message for socket.
-  const opening_message = '{"subscribe":' + JSON.stringify(tickersArray) + "}";
-
   //Sending tickers that will receive Websocket information.
   ws.onopen = function open() {
     console.log("StockSocket has opened a WebSocket Connection with Yahoo.");
-    ws.send(opening_message);
+    ws.send('{"subscribe":' + JSON.stringify(tickersArray) + "}");
   };
 
   //Receiving, decoding, and transmitting stock data to callback method.
   ws.onmessage = function incoming(data) {
-    var buffer = base64ToArray(data.data); // decode from base 64
-    var PricingData = protobuf.quotefeeder.PricingData;
-    var data = PricingData.decode(buffer); // Decode using protobuff
-    data = PricingData.toObject(data, {
-      // Convert to a JS object
-      enums: String,
-    });
-    if (tickersArray.indexOf(data.id) != -1) {
-      callback(data);
+    let decodedData = Ticker.decode(Buffer.from(data.data, "base64")).toJSON();
+    if (tickersArray.indexOf(decodedData.id) != -1) {
+      callback(decodedData);
     }
   };
 
@@ -110,32 +103,10 @@ async function startDataFeed(callback) {
   };
 }
 
-/**
- * Helper function to convert a base 64 string into a bytes array
- * @param {String} base64 a string in base 64
- */
-function base64ToArray(base64) {
-  var binaryString = atob(base64);
-  var len = binaryString.length;
-  var bytes = new Uint8Array(len);
-  for (var i = 0; i < len; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes;
-}
-
-/**
- * Helper function to convert an encoded string into base64
- * @param {String} str An encoded string
- */
-function atob(str) {
-  return Buffer.from(str, "base64").toString("binary");
-}
-
 module.exports = {
   addTickers,
   addTicker,
   removeTicker,
   removeTickers,
-  removeAllTickers,
+  removeAllTickers
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,36 +1,19 @@
-var WebSocket = require("isomorphic-ws"),
-  protobuf = require("../src/__finStreamer-proto"),
-  expect = require("chai").expect;
+var expect = require("chai").expect;
+var StockSocket = require("../src/StockSocket");
 
 describe("UT Suite checking Yahoo Socket Connection & Response", function () {
-  var ws;
   var wsData;
 
   this.beforeAll(function (done) {
     this.timeout(20000);
-    //Setup
-    ws = new WebSocket("wss://streamer.finance.yahoo.com");
-    //Open
-    ws.onopen = function open() {
-      console.log("Socket opened.");
-      ws.send('{"subscribe": ["TSLA"]}');
-    };
-    //Receive
-    ws.onmessage = function incoming(data) {
-      console.log(data.data);
-      var buffer = base64ToArray(data.data);
-      var PricingData = protobuf.quotefeeder.PricingData;
-      wsData = PricingData.decode(buffer);
-      wsData = PricingData.toObject(wsData, {
-        enums: String,
-      });
-      ws.send("close");
+
+    StockSocket.addTicker("TSLA", stockPriceChanged);
+
+    function stockPriceChanged(data) {
+      wsData = data;
+      StockSocket.removeAllTickers();
       done();
-    };
-    //Close
-    ws.onclose = function close() {
-      console.log("Socket disconnected.");
-    };
+    }
   });
 
   afterEach(function (done) {
@@ -39,65 +22,103 @@ describe("UT Suite checking Yahoo Socket Connection & Response", function () {
   });
 
   describe("Websocket Connection and Response Tests", function () {
-    it("Checking the data object type", function (done) {
+    it("Data is of type object", function (done) {
       this.timeout(20000);
       expect(wsData).to.be.an("object");
 
       done();
     });
-    it("Checking the data object length", function (done) {
+
+    it("Data is greater than length 1", function (done) {
       this.timeout(20000);
       expect(Object.keys(wsData).length).to.be.greaterThan(1);
 
       done();
     });
-    it("Checking the data object property keys", function (done) {
+    it("Data object has id property", function (done) {
       this.timeout(20000);
       expect(wsData.hasOwnProperty("id"));
+
+      done();
+    });
+    it("Data object has price property", function (done) {
+      this.timeout(20000);
       expect(wsData.hasOwnProperty("price"));
+
+      done();
+    });
+    it("Data object has time property", function (done) {
+      this.timeout(20000);
       expect(wsData.hasOwnProperty("time"));
+
+      done();
+    });
+    it("Data object has exchange property", function (done) {
+      this.timeout(20000);
       expect(wsData.hasOwnProperty("exchange"));
+
+      done();
+    });
+    it("Data object has quoteType property", function (done) {
+      this.timeout(20000);
       expect(wsData.hasOwnProperty("quoteType"));
+
+      done();
+    });
+    it("Data object has marketHours property", function (done) {
+      this.timeout(20000);
       expect(wsData.hasOwnProperty("marketHours"));
+
+      done();
+    });
+    it("Data object has changePercent property", function (done) {
+      this.timeout(20000);
       expect(wsData.hasOwnProperty("changePercent"));
 
       done();
     });
-
-    it("Checking the data object property values", function (done) {
+    it("Checking the id is TSLA", function (done) {
       this.timeout(20000);
-      //Check property values
       expect(wsData["id"]).to.be.equal("TSLA");
-      expect(wsData["price"]).to.be.greaterThan(1);
-      expect(wsData["time"]).to.not.be.null;
-      expect(wsData["exchange"]).to.not.be.null;
-      expect(wsData["quoteType"]).to.not.be.null;
-      expect(wsData["marketHours"]).to.not.be.null;
-      expect(wsData["changePercent"]).to.not.be.null;
 
       done();
     });
+    it("Checking the price is greater than 1", function (done) {
+      this.timeout(20000);
+      expect(wsData["price"]).to.be.greaterThan(1);
+
+      done();
+    });
+    it("Checking the time is not null", function (done) {
+      this.timeout(20000);
+      expect(wsData["time"]).to.not.be.null;
+
+      done();
+    });
+    it("Checking the exchange is not null", function (done) {
+      this.timeout(20000);
+      expect(wsData["exchange"]).to.not.be.null;
+
+      done();
+    });
+    it("Checking the quoteType is not null", function (done) {
+      this.timeout(20000);
+      expect(wsData["quoteType"]).to.not.be.null;
+
+      done();
+    });
+    it("Checking the marketHours is not null", function (done) {
+      this.timeout(20000);
+      expect(wsData["marketHours"]).to.not.be.null;
+
+      done();
+    });
+    it("Checking the changePercent is not null", function (done) {
+      this.timeout(20000);
+      expect(wsData["changePercent"]).to.not.be.null;
+
+      done();
+      process.exit(0);
+    });
   });
 });
-
-/**
- * Helper function to convert a base 64 string into a bytes array
- * @param {String} base64 a string in base 64
- */
-function base64ToArray(base64) {
-  var binaryString = atob(base64);
-  var len = binaryString.length;
-  var bytes = new Uint8Array(len);
-  for (var i = 0; i < len; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes;
-}
-
-/**
- * Helper function to convert an encoded string into base64
- * @param {String} str An encoded string
- */
-function atob(str) {
-  return Buffer.from(str, "base64").toString("binary");
-}
