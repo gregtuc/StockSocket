ea1b0325ea96fc6ab7b4c3bbed497f9a66d734ed
Greg <Greg.tucker1@hotmail.ca>
2022-05-17 13:59:11 -0400

    changes

---
+++ socket.js
@@ -0,0 +1,254 @@
+const protobuf = require("protobufjs");
+const WebSocket = require("isomorphic-ws");
+const root = protobuf.loadSync(__dirname + "\\proto\\PricingData.proto");
+const Ticker = root.lookupType("ticker");
+
+//nodecache to avoid making buggy custom array implementations
+const NodeCache = require("node-cache");
+const tickerCache = new NodeCache();
+
+//not const because of frequent re-instantiation in activate function
+var ws = new WebSocket("wss://streamer.finance.yahoo.com");
+
+//callback as global variable so that the user doesn't need to re-pass it everytime
+var userCallback;
+
+//to verify if the user manually shut down the socket (0 is closed)
+var toggle = 0;
+
+/**
+ * Method that adds a single ticker to the watchlist
+ * @param {String} ticker A string containing a ticker symbol
+ */
+function addTicker(ticker, callback) {
+	var err = checkTickerSyntax(ticker);
+	if (err != "") {
+		return err;
+	}
+
+	if (!tickerCache.has(ticker)) {
+		tickerCache.set(ticker, "");
+	}
+
+	//If the socket is already open, restart it to account for new tickers
+	if (ws.readyState == 1) {
+		restart();
+	}
+	start(callback);
+}
+
+/**
+ * Method that removes a specific ticker from the watchlist
+ * @param {string} ticker A string containing a ticker symbol
+ */
+function removeTicker(ticker) {
+	if (tickerCache.has(ticker)) {
+		tickerCache.del(ticker);
+	}
+	//If the socket is already open, restart it to account for removed tickers
+	if (ws.readyState == 1) {
+		restart();
+	}
+	return;
+}
+
+/**
+ * Method that adds an array of tickers to the watchlist
+ * @param {String[]} tickers An array of strings
+ */
+async function addTickers(tickers, callback) {
+	if (!Array.isArray(tickers)) {
+		throw "Tickers must be an array of strings";
+	}
+	for (let i = 0; i < tickers.length; i++) {
+		var err = checkTickerSyntax(tickers[i]);
+		if (err != "") {
+			return err;
+		}
+
+		if (!tickerCache.has(tickers[i])) {
+			tickerCache.set(tickers[i], "");
+		}
+	}
+
+	//If the socket is already open, restart it to account for new tickers
+	if (ws.readyState == 1) {
+		restart();
+	}
+	start(callback);
+}
+
+/**
+ * Method that removes an array of tickers from the watchlist
+ * @param {String[]} tickers An array of strings
+ */
+function removeTickers(tickers) {
+	if (!Array.isArray(tickers)) {
+		return "Tickers must be an array of strings";
+	}
+	for (let i = 0; i < tickers.length; i++) {
+		var err = checkTickerSyntax(tickers[i]);
+		if (err != "") {
+			return err;
+		}
+
+		if (tickerCache.has(tickers[i])) {
+			tickerCache.del(tickers[i]);
+		}
+	}
+	//If the socket is already open, restart it to account for removed tickers
+	if (ws.readyState == 1) {
+		restart();
+	}
+	return;
+}
+
+/**
+ * Method that removes every element from the tickers watchlist and ends the socket connection
+ */
+function clear() {
+	tickerCache.flushAll();
+	if (ws.readyState == 1) {
+		restart();
+	}
+}
+
+/**
+ * Method that returns the current list of tickers
+ * @returns {String[]} An array of strings containing the tickers that are currently subscribed to
+ */
+function list() {
+	return tickerCache.keys();
+}
+
+/**
+ * Method that starts the websocket connection.
+ */
+function start(callback) {
+	if (!ws) {
+		ws = new WebSocket("wss://streamer.finance.yahoo.com");
+	}
+
+	if (ws.readyState != 1) {
+		userCallback = callback;
+		toggle = 1;
+		activate();
+	} else {
+		restart();
+	}
+}
+
+/**
+ * Method that terminates the websocket connection.
+ */
+function stop() {
+	if (ws.readyState == 1) {
+		toggle = 0;
+		ws.terminate();
+	}
+}
+
+/**
+ * Method that restarts the websocket connection. Doesn't re-open connection if empty watchlist
+ */
+function restart() {
+	if (ws.readyState == 1) {
+		toggle = 0;
+		ws.terminate();
+	}
+	if (tickerCache.keys().length > 0) {
+		toggle = 1;
+		activate();
+	}
+}
+
+/**
+ * Method that starts a Websocket connection with Yahoo Finance for the tickers in the watchlist
+ */
+function activate() {
+	ws = new WebSocket("wss://streamer.finance.yahoo.com");
+
+	ws.onopen = function open() {
+		console.log(
+			`StockSocket is opening a websocket connection with tickers: ${tickerCache
+				.keys()
+				.toString()}`
+		);
+		ws.send(
+			JSON.stringify({
+				subscribe: tickerCache.keys(),
+			})
+		);
+	};
+	ws.onmessage = function incoming(message) {
+		var ticker = Ticker.decode(Buffer.from(message.data, "base64")).toJSON();
+		userCallback(ticker);
+	};
+	ws.onclose = function close() {
+		console.log("Socket closed");
+
+		//if the user didn't manually shut down the socket, restart the connection
+		if (toggle == 1) {
+			activate();
+		}
+	};
+}
+
+/**
+ * Method that checks if a ticker name is valid
+ * @param {String} ticker
+ * @returns
+ */
+function checkTickerSyntax(ticker) {
+	if (!ticker) {
+		return "Ticker is required";
+	}
+	if (typeof ticker !== "string") {
+		return "Ticker must be a string";
+	}
+	if (ticker.length === 0) {
+		return "Ticker must be a non-empty string";
+	}
+	return "";
+}
+
+module.exports = {
+	start,
+	stop,
+	clear,
+	addTicker,
+	addTickers,
+	removeTicker,
+	removeTickers,
+	list,
+};
+
+/**
+ * Method that checks if a ticker name is registered on a yahoo-listed market.
+ * @param {String} ticker
+ * @returns {boolean} true if the ticker is registered on a yahoo-listed market, otherwise false
+ */
+/*
+async function checkTickerExists(ticker) {
+	return new Promise((resolve, reject) => {
+		axios
+			.get(
+				`https://query2.finance.yahoo.com/v1/finance/search?q=${ticker}&quotesCount=1&newsCount=0`
+			)
+			.then((response) => {
+				if (
+					response.data.quotes[0] != undefined &&
+					String(response.data.quotes[0].symbol).toUpperCase() ===
+						ticker.toUpperCase()
+				) {
+					resolve(true);
+				} else {
+					resolve(false);
+				}
+			})
+			.catch((error) => {
+				resolve(error);
+			});
+	});
+}
+*/
---
+++ test/e2e_regmarket.spec.js
@@ -0,0 +1,198 @@
+var expect = require("chai").expect;
+
+describe("End-End Test (Regular-Market)", function () {
+	//If not in Regular-Market, skip the test
+	var currentTimeString = new Date().toLocaleString("en-US", {
+		timeZone: "America/New_York",
+		hour: "numeric",
+		minute: "numeric",
+		second: "numeric",
+		hour12: false,
+	});
+	var day = new Date().getDay();
+
+	if (
+		currentTimeString >= "09:30:00" &&
+		currentTimeString <= "15:59:00" &&
+		day != 6 &&
+		day != 0
+	) {
+		var stockSocket;
+		var ticker = "TSLA";
+		var wsData = "";
+
+		this.beforeAll(function (done) {
+			//This test is flaky because on slow trading days data can take > 60s
+			this.timeout(120000);
+
+			stockSocket = require("../socket");
+			stockSocket.clear();
+			stockSocket.addTicker(ticker, callback);
+
+			function callback(data) {
+				wsData = data;
+				stockSocket.clear();
+				stockSocket.stop();
+				done();
+			}
+		});
+
+		this.afterAll(function () {
+			stockSocket = "";
+		});
+
+		it("Data Object is of type object", function (done) {
+			expect(wsData).to.be.an("object");
+			done();
+		});
+
+		it("Data Object is greater than length 1", function (done) {
+			expect(Object.keys(wsData).length).to.be.greaterThan(1);
+			done();
+		});
+
+		//ID property
+		it("id property: exists", function (done) {
+			expect(wsData.hasOwnProperty("id"));
+			done();
+		});
+		it("id property: is type string", function (done) {
+			expect(typeof wsData.id === "string");
+			done();
+		});
+		it("id property: is length > 1", function (done) {
+			expect(wsData.id.length > 1);
+			done();
+		});
+		it(`id property: is equal to ${ticker}`, function (done) {
+			expect(typeof wsData.id === ticker);
+			done();
+		});
+
+		//Price property
+		it("price property: exists", function (done) {
+			expect(wsData.hasOwnProperty("price"));
+			done();
+		});
+		it("price property: is type number", function (done) {
+			expect(typeof wsData.price === "number");
+			done();
+		});
+		it("price property: is non negative", function (done) {
+			expect(wsData.price >= 0);
+			done();
+		});
+
+		//Time property
+		it("time property: exists", function (done) {
+			expect(wsData.hasOwnProperty("time"));
+			done();
+		});
+		it("time property: is type string", function (done) {
+			expect(typeof wsData.time === "string");
+			done();
+		});
+		it("time property: is not null", function (done) {
+			expect(wsData.time !== null);
+			done();
+		});
+
+		//Exchange Property
+		it("exchange property: exists", function (done) {
+			expect(wsData.hasOwnProperty("exchange"));
+			done();
+		});
+		it("exchange property: is type string", function (done) {
+			expect(typeof wsData.exchange === "string");
+			done();
+		});
+		it("exchange property: is equal to NMS", function (done) {
+			expect(wsData.exchange === "NMS");
+			done();
+		});
+
+		//QuoteType Property
+		it("quoteType property: exists", function (done) {
+			expect(wsData.hasOwnProperty("quoteType"));
+			done();
+		});
+		it("quoteType property: is type string", function (done) {
+			expect(typeof wsData.quoteType === "string");
+			done();
+		});
+		it(`quoteType property: is equal to EQUITY`, function (done) {
+			expect(wsData.quoteType === "EQUITY");
+			done();
+		});
+
+		//MarketHours Property
+		it("marketHours property: exists", function (done) {
+			expect(wsData.hasOwnProperty("marketHours"));
+			done();
+		});
+		it("marketHours property: is type string", function (done) {
+			expect(typeof wsData.marketHours === "string");
+			done();
+		});
+		it("marketHours property: is equal to REGULAR_MARKET", function (done) {
+			expect(wsData.marketHours === "REGULAR_MARKET");
+			done();
+		});
+
+		//ChangePercent Property
+		it("changePercent property: exists", function (done) {
+			expect(wsData.hasOwnProperty("changePercent"));
+			done();
+		});
+		it("changePercent property: is type number", function (done) {
+			expect(typeof wsData.changePercent === "number");
+			done();
+		});
+		it("changePercent property: is not null", function (done) {
+			expect(wsData.changePercent !== null);
+			done();
+		});
+
+		//DayVolume Property
+		it("dayVolume property: exists", function (done) {
+			expect(wsData.hasOwnProperty("dayVolume"));
+			done();
+		});
+		it("dayVolume property: is type string", function (done) {
+			expect(typeof wsData.dayVolume === "string");
+			done();
+		});
+		it("dayVolume property: is not null", function (done) {
+			expect(wsData.dayVolume !== null);
+			done();
+		});
+
+		//Change Property
+		it("change property: exists", function (done) {
+			expect(wsData.hasOwnProperty("change"));
+			done();
+		});
+		it("change property: is type number", function (done) {
+			expect(typeof wsData.change === "number");
+			done();
+		});
+		it("change property: is not null", function (done) {
+			expect(wsData.change !== null);
+			done();
+		});
+
+		//PriceHint Property
+		it("priceHint property: exists", function (done) {
+			expect(wsData.hasOwnProperty("priceHint"));
+			done();
+		});
+		it("priceHint property: is type string", function (done) {
+			expect(typeof wsData.priceHint === "string");
+			done();
+		});
+		it("priceHint property: is not null", function (done) {
+			expect(wsData.priceHint !== null);
+			done();
+		});
+	}
+});
